ea323d75aa90e456c586c480252a3dc06e443e45
Your Name <you@example.com>
2020-06-23 04:34:59 +0200

    Format: added kernel includes to pOS.h instead of kernel.cpp

--- src/pOS/include/kernel/pOS.h
+++ src/pOS/include/kernel/pOS.h
@@ -1,6 +1,17 @@
 #ifndef _POS_H_
 #define _POS_H_
 
+#include <stdio.h>
+#include <math.h>
+#include <assert.h>
+
+#include <kernel/time.h>
+#include <kernel/debug.h>
+#include <kernel/drivers.h>
+#include <kernel/interrupts/gdt.h>
+#include <kernel/interrupts/idt.h>
+#include <kernel/interrupts/isrs.h>
+
 #define DEBUG
 #define VERSION "meme"
 
