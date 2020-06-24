cafc92c531f0e6342e46508b4583d26e2dbd776c
Your Name <you@example.com>
2020-06-24 03:33:16 +0200

    Base: added outw, outl, inw and inl to the system interface

--- src/pOS/arch/x86/kernel/system.cpp
+++ src/pOS/arch/x86/kernel/system.cpp
@@ -7,9 +7,33 @@ uint8_t System::inb(uint16_t port)
         return result;
 }
 
+uint16_t System::inw(uint16_t port)
+{
+    uint16_t result;
+    asm volatile( "inw %1, %0" : "=a"(result) : "Nd"(port));
+        return result;
+}
+
+uint32_t System::inl(uint16_t port)
+{
+    uint32_t result;
+    asm volatile( "inl %1, %0" : "=a"(result) : "Nd"(port));
+        return result;
+}
+
 void System::outb(uint16_t port, uint8_t data)
 {
    asm volatile( "outb %0, %1" : : "a"(data), "Nd"(port));
+}
+
+void System::outw(uint16_t port, uint16_t data)
+{
+    asm volatile ("outw %0, %1" : : "a" (data), "Nd" (port));
+}
+
+void System::outl(uint16_t port, uint32_t data)
+{
+    asm volatile ("outl %0, %1" : : "a" (data), "Nd" (port));
 }
 
 int System::idle_loop(void)
--- src/pOS/include/kernel/system.h
+++ src/pOS/include/kernel/system.h
@@ -7,7 +7,12 @@ class System
 {
 public:
     static uint8_t inb(uint16_t port);
+    static uint16_t inw(uint16_t port);
+    static uint32_t inl(uint16_t port);
+
     static void outb(uint16_t port, uint8_t data);
+    static void outw(uint16_t port, uint16_t data);
+    static void outl(uint16_t port, uint32_t data);
 
     static int idle_loop(void);
 };
