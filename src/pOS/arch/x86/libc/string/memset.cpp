246bc94b176cd46a66437167eeaa40611d86da65
Your Name <you@example.com>
2020-06-25 04:05:00 +0200

    stdlib: fixed memset implementation bug
    BUG: memset was just setting the first value of the buf

--- src/pOS/arch/x86/libc/string/memset.cpp
+++ src/pOS/arch/x86/libc/string/memset.cpp
@@ -5,7 +5,7 @@ void* memset(void* buf, int value, size_t size)
     uint8_t* bufptr = static_cast<uint8_t*>(buf);
 
     for(size_t i = 0; i < size; i++)
-        *bufptr = (uint8_t)value;
+        bufptr[i] = (uint8_t)value;
 
     return buf;
 }
